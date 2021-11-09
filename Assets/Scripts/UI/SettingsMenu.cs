88db5b1f4dd8e67470ddef7b6471d1da04c65dbe
Marcos Engelhard <engelhardmarcos2015@gmail.com>
2021-11-09 23:41:19 +0000

    Added a few options at the main menu
    -added toggle fullscreen and sync; resolution ( testes in build as well)

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private Resolution[] resolutions;
+
+    [SerializeField]private Dropdown resolutionDropdown;
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for( int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if( resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution( int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen); ;
+    }
+    public void SetVolume( float volume)
+    {
+        Debug.Log("volume");
+        
+    }
+    
+    public void IsFullscreen( bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetSyncToogle( bool hasSync)
+    {
+        QualitySettings.vSyncCount = hasSync ? 1 : 0;
+        Debug.Log(QualitySettings.vSyncCount.ToString());
+        
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+}
