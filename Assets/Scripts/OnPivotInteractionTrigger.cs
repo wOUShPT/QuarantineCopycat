84d23088670bcb6e051eed708a897eeda23b5012
wOUShPT <61854303+wOUShPT@users.noreply.github.com>
2021-12-08 18:05:22 +0000

    Navmesh implementation
    - Navmesh implementation for player position adjustment before cutscenes/animations

---
+++ Assets/Scripts/OnPivotInteractionTrigger.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPivotInteractionTrigger : MonoBehaviour , IInteractable
+{
+    private PlayerMovement _playerMovement;
+    public Transform interactionPivot;
+    public float interactionDistance;
+    public GameEvent gameEvent;
+    private UIManager _uiManager;
+
+    public void Awake()
+    {
+        _uiManager = FindObjectOfType<UIManager>();
+        _playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    public float InteractionDistance()
+    {
+        return interactionDistance;
+    }
+
+    public void Interact()
+    {
+        _playerMovement.MoveToTarget(interactionPivot, 0.01f);
+        _playerMovement.isOnActionPivot = false;
+        StartCoroutine(WaitAndRaise());
+    }
+
+    public void ExitInteract()
+    {
+
+    }
+
+    IEnumerator WaitAndRaise()
+    {
+        _uiManager.ToggleInteractionPrompt(false);
+        yield return new WaitUntil(() => _playerMovement.isOnActionPivot);
+        gameEvent.Raise();
+        _playerMovement.isOnActionPivot = false;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,7 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
 using Cinemachine;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Timeline;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -13,7 +20,9 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController _characterController;
     [SerializeField]
     private Transform cameraPivot;
-    
+    [SerializeField] 
+    private NavMeshAgent _agent;
+    public bool isOnActionPivot;
     private Vector3 _currentMoveDirection;
     private Vector3 _currentRotation;
     private Vector3 _startHeadPosition;
@@ -22,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
     
     void Awake()
     {
+        isOnActionPivot = false;
         _currentHeadPosition = cameraPivot.localPosition;
         _startHeadPosition = _currentHeadPosition;
         _headBobTimeCounter = 0;
@@ -67,5 +77,26 @@ public class PlayerMovement : MonoBehaviour
         }
 
         cameraPivot.transform.localPosition = _currentHeadPosition;
+    }
+
+    private IEnumerator MoveToTargetCoroutine(Transform target, float distanceThreshold)
+    {
+        _agent.SetDestination(target.position);
+        yield return new WaitUntil(() => Vector3.Distance(transform.position, target.position) <= _agent.stoppingDistance + distanceThreshold);
+        while (Mathf.Abs(_currentRotation.y - target.rotation.eulerAngles.y) >= 0.005f)
+        {
+            _currentRotation.y = Mathf.Lerp(_currentRotation.y, target.rotation.eulerAngles.y, Time.deltaTime * 3f);
+            transform.rotation = Quaternion.Euler(_currentRotation);
+            yield return null;
+        }
+        isOnActionPivot = true;
+    }
+
+    public void MoveToTarget(Transform target, float distanceThreshold)
+    {
+        PlayerProperties.FreezeMovement = true;
+        PlayerProperties.FreezeAim = true;
+        PlayerProperties.FreezeInteraction = true;
+        StartCoroutine(MoveToTargetCoroutine(target, distanceThreshold));
     }
 }
